fe6a30adcfbccfdd8ccbfb2f5120cefcd585c6dd
Lillian Blair <lilliand_2000@yahoo.com>
2020-06-11 19:36:03 -0500

    Made changes to Index.js

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 // add menu toggle to bars icon in nav bar
-import { Nav, Main } from "./components";
+import { Nav, Main } from "./Components";
 import * as state from "./store";
 
+const render = () => {
+  document.querySelector("#root").innerHTML = `
+  ${Header()}
+  ${Nav()}
+  ${Main()}
+  ${Footer()}
+  `;
+}
+
+render();
 
 
 document.querySelector(".fa-bars").addEventListener("click", () => {
