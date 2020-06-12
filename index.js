0a025558fa31948208236455333624ce5df666ee
Lillian Blair <lilliand_2000@yahoo.com>
2020-06-11 19:17:37 -0500

    Added import in index.js for components and store

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 // add menu toggle to bars icon in nav bar
+import { Nav, Main } from "./components";
+import * as state from "./store";
+
+
+
 document.querySelector(".fa-bars").addEventListener("click", () => {
   document.querySelector("nav > ul").classList.toggle("hidden--mobile");
 });
